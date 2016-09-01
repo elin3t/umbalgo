021031976e1d2e9fd5a029fa17480baf9103be5c
Eliecer Daza <eliecerdaza@Eliecers-MacBook-Air.local>
2016-09-01 19:57:41 -0300

    add new vaariables to save all values and create a function to return retefuente value for a salary

--- src/Nomina.java
+++ src/Nomina.java
@@ -1,31 +1,35 @@
-//UMB Virtual - Ingeniería de Software - Semestre II - Algoritmica y Programación
+//UMB Virtual - Ingenieria de Software - Semestre II - Algoritmica y Programacion
 //Profesora: Brigitte Rodriguez Mendoza
 //Estudiosos: Bairon Jose Garcia Mendoza
 //			  Eliecer Urbano Daza Parra
 //			  Jorge  Manuel  Galindo  Yanez
-//		      Julian  David  Castañeda
+//		      Julian  David  Castaneda
 
 //Programa que calcula el valor a pagar a 5 empleados, 
-//el primero gana 2 millones, el segundo gana el 10% más que el primero, 
-//el tercero el 10% más que el segundo, y así sucesivamente hasta llegar al quinto. Se deben hacer tres rutinas.
+//el primero gana 2 millones, el segundo gana el 10% mas que el primero, 
+//el tercero el 10% mas que el segundo, y asi sucesivamente hasta llegar al quinto. Se deben hacer tres rutinas.
 //	 La primera, debe calcular el salario.
-//	 La segunda, debe calcular el valor neto a pagar considerando descuentos como retención en la fuente (puede ser 14%), seguro social y pensión.
-//	 La tercera, debe imprimir la información de cada empleado (salario, valor de la retención en la fuente, valor del seguro social, valor pensión, neto a pagar).
+//	 La segunda, debe calcular el valor neto a pagar considerando descuentos como retencion en la fuente (puede ser 14%), seguro social y pension.
+//	 La tercera, debe imprimir la informacion de cada empleado (salario, valor de la retencion en la fuente, valor del seguro social, valor pension, neto a pagar).
 
-/* Se importan librerías*/ 
+/* Se importan librerias*/ 
 
 public class Nomina {
 
 	public static void main(String[] args) {
-		Double[] salarios = new Double [5];
+		//multidimensional array para guardar [salario, retefuente, seguro, pension ]
+		Double[][] salarios = new Double [5][4];
+		Double retefuente = 0.0;
+		Double salud = 0.0;
+		Double seguro = 0.0;
 
 		Double valsal = (double) 2000000;
-		salarios[0] = valsal;
+		salarios[0][0] = valsal;
 		System.out.println("Salario Empleado 1: " + valsal);
 		for (int i = 1; i < 5; i++) {
 		    
 			valsal = calcula_salario(valsal); 
-			salarios[i] = valsal;
+			salarios[i][0] = valsal;
 			System.out.println("Salario Empleado " + (i+1) + ": " + valsal);
 		    
 		}
@@ -36,4 +40,9 @@ public class Nomina {
 		newsalario = (double) (salario * 1.1);
 		return newsalario;
 	}
+
+	static double calcula_retefuente(double salario){
+		return salario*retefuente;
+	}
+
 }
