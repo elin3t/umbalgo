18f644cbf6979faade2c661505ff39fa8e64ce83
Eliecer Daza <eliecerdaza@Eliecers-MacBook-Air.local>
2016-09-04 21:04:34 -0300

    Version 1.0 funciones completas se imprimen los datos deseados

--- src/Nomina.java
+++ src/Nomina.java
@@ -13,36 +13,66 @@
 //	 La tercera, debe imprimir la informacion de cada empleado (salario, valor de la retencion en la fuente, valor del seguro social, valor pension, neto a pagar).
 
 /* Se importan librerias*/ 
+import java.text.DecimalFormat;
 
 public class Nomina {
 
 	public static void main(String[] args) {
-		//multidimensional array para guardar [salario, retefuente, seguro, pension ]
-		Double[][] salarios = new Double [5][4];
-		Double retefuente = 0.0;
-		Double salud = 0.0;
-		Double seguro = 0.0;
-
+		//multidimensional array para guardar [salario, retefuente, salud, pension ]
+		Double[][] salarios = new Double [5][5];
 		Double valsal = (double) 2000000;
+
 		salarios[0][0] = valsal;
-		System.out.println("Salario Empleado 1: " + valsal);
 		for (int i = 1; i < 5; i++) {
-		    
-			valsal = calcula_salario(valsal); 
-			salarios[i][0] = valsal;
-			System.out.println("Salario Empleado " + (i+1) + ": " + valsal);
-		    
+			salarios[i][0] = calcula_salario(salarios[i-1][0]); 
 		}
+		for (int i = 0; i<salarios.length ; i++) {
+			salarios[i][1] = calcula_retefuente(salarios[i][0]);
+			salarios[i][2] = calcula_salud(salarios[i][0]);
+			salarios[i][3] = calcula_pension(salarios[i][0]);
+			Double salario_neto = calcula_salario_neto(salarios[i]);
+			salarios[i][4] = salario_neto;
+		}
+		imprimir_info_salarios(salarios);
+
+		return;
+	}
+
+	static void imprimir_info_salarios(Double[][] salarios){
+		System.out.println("-------------------------------------------------------------");
+		System.out.println("                         Salarios UMB                        ");
+		System.out.println("-------------------------------------------------------------");
+		System.out.println("|    Salario  |   Retefuente  | Salud   | Pension   |   Neto    |");
+		for (int i =0; i<salarios.length; i++ ) {
+			System.out.println(formatea_salario(salarios[i]));
+		}
+		System.out.println("-------------------------------------------------------------");
+		return;
+	}
+
+	static String formatea_salario(Double[] salario) {
+		DecimalFormat f = new DecimalFormat("'$'##,###,###.##");
+		return ("| " + f.format(salario[0])+"  |  " + f.format(salario[1])+"    | " + f.format(salario[2])+"    | " + f.format(salario[3])+"    |  " + f.format(salario[4])+" |");
+			
+	}
+	static double calcula_salario_neto(Double[] salario){
+		return (double) (salario[0] - salario[1] - salario[2] - salario[3]);
 	}
 
 	static double calcula_salario(double salario){
-		double newsalario;
-		newsalario = (double) (salario * 1.1);
-		return newsalario;
+		return (double) (salario * 1.1);
 	}
 
 	static double calcula_retefuente(double salario){
-		return salario*retefuente;
+		return (double) (salario * 0.14);
+	}
+
+	static double calcula_salud(double salario){
+		return (double) (salario * 0.085);
+	}
+
+	static double calcula_pension(double salario){
+		return (double) (salario * 0.12);
 	}
 
 }
